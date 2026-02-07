eb15418d4e05ecec0f3f44887554e25cf1fd511f
Ruslan Bakeev <bakeevrdev@gmail.com>
2026-02-07 18:50:13 +0300

    Добавлена шаблон парсера аргументов

---
+++ src/utils/parser.py
@@ -0,0 +1,7 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='report script')
+parser.add_argument('--files', dest='files', nargs='+', required=True)
+parser.add_argument('--report', dest='avg', required=True)
+
+args = parser.parse_args()
