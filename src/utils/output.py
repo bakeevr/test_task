5e1060b6f9ea49c12216fa30a177a1ecb46365e2
Ruslan Bakeev <bakeevrdev@gmail.com>
2026-02-07 22:23:36 +0300

    Добавлен docstring для output_table

--- src/utils/output.py
+++ src/utils/output.py
@@ -2,4 +2,7 @@ from tabulate import tabulate
 
 
 def output_table(data: list[tuple[int, str, int]], headers) -> None:
+    """
+    Функция выводит форматированные данные в stdout
+    """
     print(tabulate(data, headers=headers, tablefmt="simple", numalign="right", stralign="left"))
